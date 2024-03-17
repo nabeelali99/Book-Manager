bef0425020e5e7e66b0d26eec605738df4b645e9
Nabeel Ali <nabeelhell@gmail.com>
2024-03-17 14:00:26 +0530

    Changed CORS

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,9 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    credentials: true,
+    origin: true,
+    // origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
   })
